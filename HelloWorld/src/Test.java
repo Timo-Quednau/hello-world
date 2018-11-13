e0fbcb980b367abd32bf2603b18f4bd0dd9c5cf2
Timo-Quednau <45000358+Timo-Quednau@users.noreply.github.com>
2018-11-13 16:02:28 +0100

    Add files via upload

---
+++ HelloWorld/src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String y = "Test";
+		String x = "30";
+		int z;
+		z = Integer.parseInt(x);
+		System.out.println("Hello World" + x + y + z);
+		int v1 = 3;
+		int v2 = 2;
+		int v3 = v1/v2;
+		double v4 = 3;
+		double v5 = 2;
+		double v6 = v4/v5;
+		System.out.println(v3);
+		System.out.println(v6);
+	}
+
+}
